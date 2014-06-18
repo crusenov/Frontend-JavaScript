3e87a96645758f83c50ba161a31cc3d02f3f63f6
Cvetan <crusenov@gmail.com>
2014-06-18 13:15:57 +0300

    add numericSort function

--- week2/exam/1/beerAndFries.js
+++ week2/exam/1/beerAndFries.js
@@ -1,13 +1,18 @@
 "use strict";
 
 var zip = function() {
+
     var values = [].slice.apply(arguments);
-    //console.log(values);
+
     return values[0].map(function(_,i) {
       return values.map(function(arr){
         return arr[i];
       });
     });
+};
+
+var numericSort = function(a, b) {
+  return a - b;
 };
 
 var mult = function(arr){
@@ -32,12 +37,8 @@ var beerAndFries = function(info){
     }
   });
 
-  beerArr.sort(function(a, b){
-    return a - b;
-  });
-  friesArr.sort(function(a, b){
-    return a - b;
-  });
+  beerArr.sort(numericSort);
+  friesArr.sort(numericSort);
 
   var beerAndFries = zip(beerArr, friesArr),
       res = 0;
