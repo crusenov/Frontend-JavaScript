21dd82b44a92368cd140c91401fbd4f28594437a
Cvetan <crusenov@gmail.com>
2014-08-08 14:49:52 +0300

    adding new feature

--- week1/students-system/client/app.js
+++ week1/students-system/client/app.js
@@ -1,22 +1,97 @@
 "use strict";
 
 $(document).ready(function() {
-  alert("Hooray, everything runs ok. You can remove this annoying alert from the code.");
+  //alert("Hooray, everything runs ok. You can remove this annoying alert from the code.");
+
+  var allStudents = [];
+
+  var groupBy = function(groupingFunction, arr) {
+        var res = {};
+        arr.forEach(function(x){
+            if(res[groupingFunction(x)]){
+              res[groupingFunction(x)].push(x);
+            } else {
+              res[groupingFunction(x)] = [x];
+        }});
+      return res;
+      };
+
+  var generateTable = function(students){
+
+    var studentsContent = "";
+    students.forEach(function(student) {
+        studentsContent += ["<tr id=\"", student.id.toString(), "\">",
+                              "<td>", student.id, "</td>",
+                              "<td>", student.name, "</td>",
+                              "<td>", student.course, "</td>",
+                            "</tr>"].join("");
+      });
+
+      var table = ["<table class='table'>",
+                   "<thead>",
+                     "<tr>",
+                       "<th>", "Id", "</th>",
+                       "<th>", "Name", "</th>",
+                       "<th>", "Course", "</th>",
+                     "</tr>",
+                   "</thead>",
+                   "<tbody>",
+                   studentsContent,
+                   "</tbody>",
+                   "</table>"].join("");
+    return table;
+  };
+
+  var find = function(predicate, arr) {
+    var newArr = arr.filter(predicate);
+    return newArr[0];
+  };
+
+  var getId = function(searchName, list){
+    var id;
+    list.forEach(function(x){
+      if(searchName === x.name && x.id >= 0){
+        id = x.id;
+      }
+    });
+    return id;
+  };
+
 
   $.getJSON('http://localhost:3000/students', function(students, textStatus) {
-      console.log(textStatus);
-      console.log(students);
+      //console.log(textStatus);
+      //console.log(students);
+      allStudents = students;
 
-      alert("Students came. Open your console and remove this alert!");
-      //start here
+      var table = generateTable(students);
+      $(".table-container").append(table);
   });
 
   $("#group-btn").on("click", function() {
-    alert("What are you looking at? Go implement that logic.");
+
+    var groupStudents = groupBy(function(student){
+      return student.course;
+    }, allStudents);
+
+    Object.keys(groupStudents).forEach(function(key){
+      var curStudent = groupStudents[key];
+      var table = generateTable(curStudent);
+
+      $(".table-container").append(table);
+    });
   });
 
   $("#search-btn").on("click", function() {
     var searched = $("#search-box").val();
-    alert("You searched for " + searched);
+    if(find(function(x) {
+      return x.name === searched;
+    }, allStudents)){
+
+      var searchedId = getId(searched, allStudents);
+      var tr = document.getElementById(searchedId);
+      tr.setAttribute('class', 'success');
+      //tr.addClass("success");
+    }
+
   });
 });
