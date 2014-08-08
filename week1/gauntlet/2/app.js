1f82c27fa4d3d2324ee4ec42f96f41a16815503b
Cvetan <crusenov@gmail.com>
2014-08-08 14:48:57 +0300

    adding new feature

--- week1/gauntlet/2/app.js
+++ week1/gauntlet/2/app.js
@@ -1,1 +1,21 @@
-console.log("Empty I am.");
+"use strict";
+//console.log("Empty I am.");
+
+$(document).ready(function(){
+  var HIGHLIGHT = "highlight",
+      allParagraphs = [".first", ".second", ".third"];
+      //allParagraphs = [];
+
+  // $("p").each(function(index, elem){
+  //   allParagraphs.push($(elem).attr("class"));
+  // });
+
+  $("#toggle-btn").on("click", function(){
+
+    $("."+HIGHLIGHT).removeClass(HIGHLIGHT);
+    var toHighlight = allParagraphs.shift(); // "." +
+    $(toHighlight).addClass(HIGHLIGHT);
+    allParagraphs.push(toHighlight);
+  });
+
+});
