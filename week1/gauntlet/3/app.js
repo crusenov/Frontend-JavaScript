b11af8f7bddb8f32b1e5aece1b7efca819b7568a
Cvetan <crusenov@gmail.com>
2014-08-08 14:49:23 +0300

    adding new feature

--- week1/gauntlet/3/app.js
+++ week1/gauntlet/3/app.js
@@ -1,3 +1,19 @@
+"use strict";
+
 $(document).ready(function() {
+
+  $("#search-button").on("click", function(){
+    var imageUrl = $("#search-input").val();
+    var $img = $("<img>").attr("src", imageUrl);
+
+    $img.load(function(){
+      $("#galery").append($img);
+      $("#galery").append("</br>");
+    });
+
+    $img.on("click", function(){
+      $img.remove();
+    });
+  });
 
 });
