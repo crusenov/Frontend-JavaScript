75485a91fc605942d92c00fb22263b88618c9cce
Cvetan <crusenov@gmail.com>
2014-08-17 12:03:57 +0300

    added feature

--- exam2/1-corrupted-data/app.js
+++ exam2/1-corrupted-data/app.js
@@ -40,7 +40,7 @@ var corruptedData = function(data){
     });
 
     countLog = studentsForDate.reduce(function (acc, curr) {
-      if (typeof acc[curr] == "undefined") {
+      if (typeof acc[curr] === "undefined") {
         acc[curr] = 1;
       } else {
         acc[curr] += 1;
